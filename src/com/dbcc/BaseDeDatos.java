3c31e2e0f8698d8575fcaa1c9477013dc2186e16
arielmerin <arielmerino@ciencias.unam.mx>
2020-03-24 19:40:44 -0600

    repo

--- src/com/dbcc/BaseDeDatos.java
+++ src/com/dbcc/BaseDeDatos.java
@@ -9,9 +9,23 @@ import java.io.IOException;
 import java.io.Serializable;
 import java.util.Scanner;
 
-
+/**
+ * <h1>Base de Datos</h1>
+ * Dentro de esta clase se van a simular las veces de una base de datos que tiene alumnos y materias
+ * Se encarga de leer un archivo y darle formato a la informacion que contiene para realacionarla con alumnos
+ * y sus respectivas materias
+ * @author Ariel, Armando
+ * @version 1
+ */
 public class BaseDeDatos implements Serializable {
+    /**
+     * Esta sera la primera lista que tengamos, donde los datos estan sin procesar y seguramente
+     * habra elementos repetidos o no bien relacionados
+     */
     private static Lista<Alumno> listaMadre = new Lista<>();
+    /**
+     * En esta estructura almacenaremos los nombres de las mataerias sin repetir
+     */
     private  Lista<Materia> listaMaterias = new Lista<>();
     /**
      * Esta lista albergara los alumnos sin repetir, con las materias correspondietnes
@@ -19,7 +33,12 @@ public class BaseDeDatos implements Serializable {
     private Lista<Alumno> listaFinalF = new Lista<>();
     private static int contador;
 
-
+    /**
+     * En este metodo se toma el path que se indica al principio para crear un flujo de lectura donde en cada linea que
+     * encuentre dividira la linea en dos, la primera parte antes de que aparezca una coma y la segunda viene despues de
+     * la coma, ademas esta pensado para que las lineas esten entre parentesis por lo que los elimina
+     * @param ruta direccion del archivo a leer
+     */
     public void leyendo(String ruta){
         try {
             Scanner input = new Scanner(new File(ruta));
@@ -45,6 +64,12 @@ public class BaseDeDatos implements Serializable {
         }
     }
 
+    /**
+     * En este metodo se busca cada uno de los alumnos que se recolecto el la lista principal y luego lo regeresa la materia
+     * con los alumnos tantas veces como haya encontrado los alumnos
+     * @param nombreMateria la materia con la que buscara relacionar a los alumnos
+     * @return materia con todos los alumnos que encontro
+     */
     public Materia asignaAlumnos(String nombreMateria){
         Materia materia = new Materia(nombreMateria);
         for (Alumno al: listaMadre) {
@@ -55,6 +80,10 @@ public class BaseDeDatos implements Serializable {
         return materia;
     }
 
+    /**
+     *
+     * @return
+     */
     public Lista<String> alumnosSinRepetir(){
         Lista<String> alumnosSinRepetir = new Lista<>();
         for (Alumno estudiante: listaMadre) {
